4969a383363bd9293b999134483a5ec6f106a25d
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-06-06 01:12:34 +0000

    ジャンル機能実装

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,40 @@
 class Admin::GenresController < ApplicationController
+  #before_action :authenticate_admin!
+  before_action :ensure_genre, only: [:edit, :update]
+
   def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+  
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      render :index
+    end
+  end
+  
+  def update
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
   end
 
   def edit
+  end
+  
+  private
+  
+  def ensure_genre
+    @genre = Genre.find(params[:id])
+  end
+  
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,6 @@ class Genre < ApplicationRecord
   
   has_many :posts, dependent: :destroy
   
+  validates :name, presence: true, uniqueness: true
   
 end
