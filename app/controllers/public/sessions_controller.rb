f828f0f22f18e1f5e59da41df35fd7246995ed01
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-06-06 10:27:03 +0000

    ログアウト機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :reject_inactive_customer, only: [:create]
+  before_action :reject_inactive_user, only: [:create]
 
 
   # GET /resource/sign_in
@@ -28,11 +28,23 @@ class Public::SessionsController < Devise::SessionsController
   # end
   
   def after_sign_in_path_for(resource)
-    mypage_path
+    my_page_path
   end
 
   def after_sign_out_path_for(resource)
     root_path
   end
+  
+   def reject_inactive_user
+    @user = User.find_by(email: params[:user][:email])
+    if @user
+      if @user.valid_password?(params[:user][:password]) && !@user.is_active
+        flash[:danger] = 'お客様は退会済みです。申し訳ございませんが、別のメールアドレスをお使いください。'
+        redirect_to new_user_registration_path
+      end
+    end
+   end
+
+  
 
 end
