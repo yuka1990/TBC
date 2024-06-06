4e761260f7ba47f89b1aea4dcc83a9b661f4eccb
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-06-06 10:01:01 +0000

    ログアウト以外のログイン機能まで

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,8 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_inactive_customer, only: [:create]
+
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +26,13 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  def after_sign_in_path_for(resource)
+    mypage_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
 end
