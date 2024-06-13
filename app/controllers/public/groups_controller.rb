5dd5c78083ebe256637ae7d7a71d2009b3166fbc
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-06-13 08:03:39 +0000

    グループ機能

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -1,21 +1,23 @@
 class Public::GroupsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_group, only: [:edit, :update, :show, :destroy]
-  
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def new
     @group = Group.new
   end
-  
+
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
     if @group.save
-      redirect_to groups_path
+      redirect_to groups_path, notice: "Successfully saved."
     else
+      flash[:alert] = "Failed to save."
       render :new
     end
   end
-  
+
   def update
     if @group.update(group_params)
       redirect_to groups_path, notice: "Successfully saved."
@@ -35,20 +37,25 @@ class Public::GroupsController < ApplicationController
   def edit
   end
   
+  def destroy
+    @group.destroy
+    redirect_to groups_path
+  end
+
   private
-  
+
   def ensure_group
     @group = Group.find(params[:id])
   end
-  
+
   def ensure_correct_user
     unless @group.owner_id == current_user.id
       redirect_to groups_path
     end
   end
-  
+
   def group_params
     params.require(:group).permit(:name, :introduction)
   end
-  
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,11 +3,26 @@ class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
   
+  
   validates :name, presence: true
   validates :introduction, presence: true
   
   def is_owned_by?(user)
     owner_id == user.id
+  end
+  
+  def owner_nickname
+    owner.nickname
+  end
+  
+  def includesUser?(user)
+    group_users.exists?(user_id: user.id)
+  end
+
+  private
+
+  def owner
+    User.find_by(id: owner_id)
   end
   
 end
